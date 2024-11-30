4261757a9e07ede00e97c269c5adfabb7be36385
nusretyalcn <yalcnusret@gmail.com>
2024-12-01 00:44:41 +0300

    fileHelper Bug fix

--- Business/Constants/PathConstants.cs
+++ Business/Constants/PathConstants.cs
@@ -2,5 +2,5 @@ namespace Business.Constants;
 
 public class PathConstants
 {
-    public static string ImagesPath = "wwwroot\\Uploads\\Images\\";
+    public static string ImagesPath = "wwwroot//Uploads//Images//";
 }
