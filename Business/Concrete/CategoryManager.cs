b4581be0e9af32535cabcc0c0370166bacac27b0
nusretyalcn <yalcnusret@gmail.com>
2024-12-18 23:52:19 +0300

    Category Service refactoring

--- Business/Concrete/CategoryManager.cs
+++ Business/Concrete/CategoryManager.cs
@@ -1,5 +1,10 @@
 using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -15,28 +20,41 @@ public class CategoryManager:ICategoryService
         _categoryDal = categoryDal;
     }
     
+    [CacheAspect]
     public IDataResult<List<Category>> GetAll()
     {
         return new SuccessDataResult<List<Category>>(_categoryDal.GetAll(),Messages.CategoriesListed);
     }
 
+    [CacheAspect]
     public IDataResult<Category> GetById(int id)
     {
         return new SuccessDataResult<Category>(_categoryDal.Get(p=>p.Id==id));
     }
 
+    [SecuredOperation("admin")]
+    [ValidationAspect(typeof(CategoryValidator))]
+    [CacheRemoveAspect("ICategoryService.Get")]
+    [TransactionScopeAspect]
     public IResult Add(Category category)
     {
         _categoryDal.Add(category);
         return new SuccessResult(Messages.CategoryAdded);
     }
 
+    [SecuredOperation("admin")]
+    [ValidationAspect(typeof(CategoryValidator))]
+    [CacheRemoveAspect("ICategoryService.Get")]
+    [TransactionScopeAspect]
     public IResult Update(Category category)
     {
         _categoryDal.Update(category);
         return new SuccessResult(Messages.CategoryUpdated);
     }
 
+    [SecuredOperation("admin")]
+    [CacheRemoveAspect("ICategoryService.Get")]
+    [TransactionScopeAspect]
     public IResult Delete(Category category)
     {
         _categoryDal.Delete(category);
---
+++ Business/ValidationRules/FluentValidation/CategoryValidator.cs
@@ -0,0 +1,13 @@
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation;
+
+public class CategoryValidator:AbstractValidator<Category>
+{
+    public CategoryValidator()
+    {
+        RuleFor(p => p.CategoryName).MinimumLength(2);
+        RuleFor(p => p.CategoryName).NotEmpty();
+    }
+}
